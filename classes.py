ad2421117680e6bdf47d4a7839c639eb997cd118
k28-singh <karanraj2.singh@live.uwe.ac.uk>
2023-01-11 10:58:08 +0000

    first commit

---
+++ classes.py
@@ -0,0 +1,19 @@
+class Directory:
+    def __init__(self, **args):
+        self.parent
+        self.children
+        self.directory
+        return (self)
+
+
+class File:
+    def __init__(self, **args):
+        # a file probably doesnt need a parent as it is accessed from parent variable itself as in, directory.children[0].name
+        # where directory IS the parent.
+        # self.parent
+        self.name
+        self.size
+        self.format
+        self.dateAdded
+        self.dateModified
+        return (self)
