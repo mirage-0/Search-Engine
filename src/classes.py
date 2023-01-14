d2ad47440987bca5eb3c7ace3b0356730cd59ba7
k28-singh <karanraj2.singh@live.uwe.ac.uk>
2023-01-14 12:54:49 +0000

    1

--- src/classes.py
+++ src/classes.py
@@ -2,9 +2,9 @@ class Directory:
     def __init__(self, **args):
         self.valueList  # = list of values from args
     # holds all directories Paths in system
-        self.dir
+        self.dictDirPath
     # holds all files Paths in system
-        self.files
+        self.dictFilesPath
     # holds the format keys we will generate (mp4, mp3, exe, txt, word,.)
         self.formatsKeys = []
     # holds the name keys we will generate (encoded number 1, encoded number 2, encoded number 3,.)
@@ -14,14 +14,16 @@ class Directory:
     # first dict, divides ds in directories and files
         self.dictA = {0: self.dictDir, 1: self.dictFile}
     # directory dict which divides ONLY directories via similar file names
-        self.dictDir = self.generateDictionary(self.dirNameKeys, self.dir)
+        self.dictDir = self.generateDictionary(
+            self.dirNameKeys, self.dictDirPath)
     # file dict, divides ds in formats
         self.dictFile = self.generateDictionary(self.formatsKeys, self.dictC)
     # third dict, divides ds by names
         # 'encoding number 1' holds compressed data about the names of the files it holds, just by sending encoded
         # number to a special function decode() we get a general idea about what file names to expect in
         # the dict this key 'encoding number 1' holds, then decide if its worth it to search through all of the dict
-        self.dictC = self.generateDictionary(self.fileNameKeys, self.files)
+        self.dictC = self.generateDictionary(
+            self.fileNameKeys, self.dictFilesPath)
 
         return (self)
 
